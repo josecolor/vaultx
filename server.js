89be5642d5a67941feef10bbb349e38605b9e649
josecolor <jose.colorvision@gmail.com>
2026-07-27 20:09:44 -0400

    Fix: trust proxy para Railway (soluciona ERR_ERL_UNEXPECTED_X_FORWARDED_FOR)

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const crypto = require('crypto');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 const pool = new Pool({
